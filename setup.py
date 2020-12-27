8fa0897af1d0de070481e7945ec69bfd34edf33a
sptkl <caobaiyue@gmail.com>
2020-12-27 18:16:23 -0500

    strict requirements

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(
     name="src",
     version="0.1",
     packages=["src"],
-    install_requires=["pandas", "numpy", "streamlit", "plotly"],
+    install_requires=[
+        "pandas==1.1.2", 
+        "numpy==1.19.1", 
+        "streamlit==0.71.0", 
+        "plotly==4.13.0"],
     zip_safe=False,
 )
